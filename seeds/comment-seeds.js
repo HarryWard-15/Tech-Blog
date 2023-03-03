08d1c25ecdc0a7610fa43c02b9b9967789f4c51b
Harry_Desktop <hward.1508@gmail.com>
2023-03-03 15:14:08 +0800

    MVP almost working, fewe routing and data retrievals not working

--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -1,19 +1,19 @@
 const { Comment } = require('../models');
 
 const commentData = [{
-        comment_text: "Lorem ipsum dolor sit amet",
-        user_id: 1,
-        post_id: 1
+        commentContent: "Lorem ipsum dolor sit amet",
+        userId: 1,
+        postId: 1
     },
     {
-        comment_text: "consectetur adipiscing elit",
-        user_id: 2,
-        post_id: 2
+        commentContent: "consectetur adipiscing elit",
+        userId: 2,
+        postId: 2
     },
     {
-        comment_text: "sed do eiusmod tempor incididunt",
-        user_id: 3,
-        post_id: 3
+        commentContent: "sed do eiusmod tempor incididunt",
+        userId: 3,
+        postId: 3
     }
 ];
 
